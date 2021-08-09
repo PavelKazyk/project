69a968d83b66280cf02807e3fbc1c2c96716c6ee
Павел Казык <you@example.com>
2021-08-09 23:09:11 +0300

    first commit

---
+++ JS/skript.js
@@ -0,0 +1,58 @@
+"use strict";
+
+//                                  'Примеры записей переменных (ящиков)';
+// // let number = 5;                    
+// // const LeftBorderWidth = 1;
+
+// // number = 10;
+// // console.log(number);
+//                              'Все что в кавычках - свойства или методы(действия)';
+// // const obg = {
+// //     a: 50
+// // };
+
+// // console.log(ghj);
+//                                          'Булевые значения';
+// // let wrong;
+// // console.log(wrong);
+// //  const bool = false;
+// //  const bull= true;
+// //  console.log(bool);
+//                                   "Информация в коробке";
+// const obj = {
+//     name: 'Pavel',
+//     age: 26,
+//     isMarried: false
+// };
+// console.log(obj.age);
+//                  'Массивы, порядок -автоматический, с 0';
+// let arr = ['plumb.png', 6, 'apple.bmp', [] ];
+// console.log(arr[1]);
+
+//                  'alert-всплывающее окно, confirm - всплывает да\нет,';
+//                  'prompt-всплывает для ввода ответа, +prompt - делает числом переменную';
+// alert("hello");
+// const conf = confirm('Are you here?');
+// console.log(conf);
+
+// const answer = +prompt("Вам есть 18?", "да");
+// console.log(answer+7);
+
+// const answers = [];
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+// console.log(typeof(answers));
+
+// console.log(typeof(null));   
+// // - Ошибка, быводиться как объект
+//                                  'Интерполяция - вставка переменной в строку через ${}';
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+const uzer = 'Ivan';
+
+console.log(`Привет, ${uzer}`);
+
+alert(`Привет, мой друг, ${uzer}`);
